ad05c6004003b6423f24c49c0cb4a047ac0cf640
Rustemhak <rustemhak@mail.ru>
2021-10-15 22:18:29 +0300

    solved task1 - created parent and child processes

---
+++ task1/child.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from os import getpid, _exit
+from sys import argv
+from time import sleep
+from random import randint
+
+s = int(argv[1])
+print('Запущена программа Child в процессе с PID {0} с аргументом {1}'.format(getpid(), s))
+sleep(s)
+print('Завершился процесс с PID {0}'.format(getpid()))
+_exit(randint(0, 1))
---
+++ task1/parent.py
@@ -0,0 +1,20 @@
+from os import fork, getpid, wait, execv
+from sys import argv
+from random import randint
+
+n = int(argv[1])
+child = fork()
+i = 0
+while True:
+    if child == 0:
+        number = randint(5, 10)
+        print('Сгенерированное число секунд: {0}'.format(number))
+        execv('/usr/bin/python3', ['python3', 'child.py', str(number)])
+    else:
+        ret = wait()
+        print('Дочерний процесс с PID {0} завершился. Статус завершения {1}.'.format(ret[0], ret[1]))
+        i += 1
+        if i < n:
+            child = fork()
+        else:
+            break
